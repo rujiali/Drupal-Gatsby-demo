08de8324d9949ce3962b297a6b5ee6aa22a300ad
Joshua Li <oasislrjjrl@gmail.com>
2018-09-18 13:56:25 +1000

    Removed unused files.

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,6 @@ module.exports = {
         //trackingId: `ADD YOUR TRACKING ID HERE`,
       },
     },
-    `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
